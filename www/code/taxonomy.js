e1b706a66be48330f1ab26cb4d3acda5dbc8e3a8
Peter Bremer <peter.bremer@gmail.com>
2016-03-15 17:06:27 +0100

    Shared code for writing output to page

--- www/code/taxonomy.js
+++ www/code/taxonomy.js
@@ -54,11 +54,7 @@ function buildInfoBiocaching(taxonData) {
 	console.log(taxonData);
 	var name = taxonData.hits[0]._source.scientific_name;
 	name = name.charAt(0).toUpperCase() + name.slice(1);
-	if (id != rootId) {
-		document.querySelector("title").textContent = name + " - Biocaching";
-		document.querySelector("h1").textContent = name;
-	}
-	document.querySelector("#name").textContent = name;
+	buildPage({name: name})
 
 	var xhrParent = new XMLHttpRequest();
 	xhrParent.open("GET", taxaUrlBiocaching + taxonData.hits[0]._source.parent_id, true);
@@ -77,32 +73,28 @@ function buildInfoBiocaching(taxonData) {
 
 function buildParentBiocaching(data) {
 	console.log(data);
-	if (data.hits.length == 0)
-		return;
+	var parent;
 
-	var name = data.hits[0]._source.scientific_name;
-	name = name.charAt(0).toUpperCase() + name.slice(1);
-	document.querySelector("#parent").textContent += name;
-	document.querySelector("#parent").href = uri.setSearch({id: data.hits[0]._id});
+	if ((data.hits.length == 0) && (id != rootId)) {
+		data.hits = [{_source: {scientific_name: "biota"}, _id: rootId}];
+	}
+	if (data.hits.length > 0) {
+		var name = data.hits[0]._source.scientific_name;
+		name = name.charAt(0).toUpperCase() + name.slice(1);
+		parent = {name: name, id: data.hits[0]._id};
+	}
+	buildPage({parents: [parent]});
 }
 
 function buildListBiocaching(data) {
 	//console.log(data);
-	var childItemTemplate = document.querySelector("#subitems li");
-	data.hits.forEach(function(taxon){
-		var item = childItemTemplate.cloneNode(true);
-		var name = taxon._source.scientific_name;
+	var children = [];
+	data.hits.forEach(function(child){
+		var name = child._source.scientific_name;
 		name = name.charAt(0).toUpperCase() + name.slice(1);
-		item.querySelector("h2").textContent = name;
-		item.querySelector("a").href = uri.setSearch({id: taxon._id});
-		childItemTemplate.parentNode.appendChild(item);
+		children.push({name: name, id: child._id});
 	});
-	if (data.hits.length == 0) {
-		var children = document.querySelector("#subitems");
-		children.parentNode.removeChild(children);
-	} else {
-		childItemTemplate.parentNode.removeChild(childItemTemplate);
-	}
+	buildPage({children: children})
 }
 
 
@@ -111,9 +103,25 @@ function buildListBiocaching(data) {
 // http://eol.org/api
 // http://eol.org/api/hierarchy_entries/1.0/51521763.json?callback=test
 
+// http://eol.org/api/hierarchy_entries/1.0/<id>.json?callback=<func> --> name
+
 function loadDataEol() {
 	if (id == rootId) {
-		buildList(null);
+		buildList({
+			scientificName: "Biota",
+			taxonConceptID: rootId,
+			ancestors: [],
+			children: [
+				{ taxonID: 51521761, taxonConceptID:    1, scientificName: "Animalia" },
+				{ taxonID: 52744048, taxonConceptID: 5559, scientificName: "Fungi" },
+				{ taxonID: 52800975, taxonConceptID:  281, scientificName: "Plantae" },
+				{ taxonID: 53103686, taxonConceptID:  288, scientificName: "Bacteria" },
+				{ taxonID: 53112249, taxonConceptID: 3352, scientificName: "Chromista" },
+				{ taxonID: 53114002, taxonConceptID: 5006, scientificName: "Viruses" },
+				{ taxonID: 53116999, taxonConceptID: 4651, scientificName: "Protozoa" },
+				{ taxonID: 53131235, taxonConceptID: 7920, scientificName: "Archaea" }
+			]
+		});
 		return;
 	}
 
@@ -124,73 +132,34 @@ function loadDataEol() {
 }
 
 function buildList(data) {
-
-	if (id == rootId) data = 
-		{
-			"scientificName": "Biota",
-			"taxonConceptID": 0,
-			"ancestors": [],
-			"children": [
-				{ "taxonID": 51521761, "taxonConceptID":    1, "scientificName": "Animalia" },
-				{ "taxonID": 52744048, "taxonConceptID": 5559, "scientificName": "Fungi" },
-				{ "taxonID": 52800975, "taxonConceptID":  281, "scientificName": "Plantae" },
-				{ "taxonID": 53103686, "taxonConceptID":  288, "scientificName": "Bacteria" },
-				{ "taxonID": 53112249, "taxonConceptID": 3352, "scientificName": "Chromista" },
-				{ "taxonID": 53114002, "taxonConceptID": 5006, "scientificName": "Viruses" },
-				{ "taxonID": 53116999, "taxonConceptID": 4651, "scientificName": "Protozoa" },
-				{ "taxonID": 53131235, "taxonConceptID": 7920, "scientificName": "Archaea" }
-			]
-		};
-	else data.ancestors.unshift({"taxonID":0,"scientificName":"Biota"});
-
 	var getDetailsFor = [];
 
-	if (id != rootId) {
-		document.querySelector("title").textContent = data.scientificName + " - Biocaching";
-		document.querySelector("h1").textContent = data.scientificName;
+
+	// retreive details for current species
+	if (id == rootId)  {
+		buildDetailsEol([{"0": {
+			taxonConcepts: [{nameAccordingTo: "Species 2000 & ITIS Catalogue of Life: April 2013", identifier: rootId}],
+			dataObjects: [{eolMediaURL: "https://upload.wikimedia.org/wikipedia/commons/d/da/Ruwenpflanzen.jpg"}], 
+			vernacularNames: []
+		}}]);
+	} else {
+		// inject biota root into ancestors tree
+		data.ancestors.unshift({taxonID: rootId, scientificName: "Biota"});
 	}
-	document.querySelector("#name").textContent = data.scientificName;
-	document.querySelector("body").id = "t" + data.taxonConceptID;
 	getDetailsFor.push(data.taxonConceptID);
 
-	var parentItemTemplate = document.querySelector("#parents div");
-	var parentItem = null;
-	data.ancestors.forEach(function(elm){
-		var item = parentItemTemplate.cloneNode(true);
-		item.querySelector("a").textContent = elm.scientificName;
-		item.querySelector("a").href = uri.setSearch({id: elm.taxonID});
-		if (parentItem == null) {
-			parentItemTemplate.parentNode.appendChild(item);
-		} else {
-			parentItem.appendChild(item);
-		}
-		parentItem = item;
+	var ancestors = [], children = [];
+	data.ancestors.forEach(function(elm) {
+		ancestors.push({name: elm.scientificName, id: elm.taxonID});
 	});
-	if (data.ancestors.length == 0) {
-		var parents = document.querySelector("#parents");
-		parents.parentNode.removeChild(parents);
-	} else {
-		parentItemTemplate.parentNode.removeChild(parentItemTemplate);
-	}
-
-	var childItemTemplate = document.querySelector("#subitems li");
 	data.children.forEach(function(elm){
-		var item = childItemTemplate.cloneNode(true);
-		item.id = "t" + elm.taxonConceptID;
-		item.querySelector("h2").textContent = elm.scientificName;
-		item.querySelector("a").href = uri.setSearch({id: elm.taxonID});
-		childItemTemplate.parentNode.appendChild(item);
+		children.push({name: elm.scientificName, id: elm.taxonID});
 		getDetailsFor.push(elm.taxonConceptID);
 	});
-	if (data.children.length == 0) {
-		var children = document.querySelector("#subitems");
-		children.parentNode.removeChild(children);
-	} else {
-		childItemTemplate.parentNode.removeChild(childItemTemplate);
-	}
+	buildPage({name: data.scientificName, ancestors: ancestors, children: children});
 
 	var script = document.createElement("script");
-	script.src = "http://eol.org/api/pages/1.0.json?batch=true&id=" + getDetailsFor.join() + "&images=1&videos=0&text=0&details=true&taxonomy=false&common_names=true&cache_ttl=300&callback=buildDetails";
+	script.src = "http://eol.org/api/pages/1.0.json?batch=true&id=" + getDetailsFor.join() + "&images=1&videos=0&text=0&details=true&taxonomy=true&common_names=true&cache_ttl=300&callback=buildDetails";
 	document.body.appendChild(script);
 	document.body.removeChild(script);
 }
@@ -198,41 +167,120 @@ function buildList(data) {
 function buildDetails(data) {
 	console.log(data);
 
-	if (id == rootId) {
-		data.push({"0": {"dataObjects": [{"eolMediaURL":"https://upload.wikimedia.org/wikipedia/commons/d/da/Ruwenpflanzen.jpg"}]}});
-	}
+	data.forEach(function(elm){
+		// each array element is an object with only one property (named by taxonConceptID); get the contents of this propery
+		var taxo = elm[Object.keys(elm)[0]];
+		console.log("taxo: ", taxo);
 
-	data.forEach(function(taxo){
-		var taxonConceptID = Object.keys(taxo)[0];
-		console.log(taxonConceptID);
-		if (taxo[taxonConceptID].dataObjects.length > 0) {
-			var elm = document.querySelector("li#t" + taxonConceptID);
-			if (elm != null) {
-				elm.querySelector("img").src = taxo[taxonConceptID].dataObjects[0].eolThumbnailURL;
-				var found = false;
-				for (i = 0; i < taxo[taxonConceptID].vernacularNames.length && !found; i++) {
-					console.log(i, taxo[taxonConceptID].vernacularNames[i].language);
-					if (taxo[taxonConceptID].vernacularNames[i].language == "en") {
-						elm.querySelector("h2").textContent += " (" + taxo[taxonConceptID].vernacularNames[i].vernacularName + ")"
-						found = true;
-					}
+		// find COL id
+		var ColID = null;
+		for (var c in taxo.taxonConcepts) {
+			if (taxo.taxonConcepts[c].nameAccordingTo == "Species 2000 & ITIS Catalogue of Life: April 2013") {
+				ColID = taxo.taxonConcepts[c].identifier;
+				break;
+			}
+		};
+
+		// find thumbnail
+		var thumbnailUrl = null, mediaUrl = null;
+		// check if there is data objects
+		if (taxo.dataObjects.length > 0) {
+			// find first data object that has a thumbnail
+			for (var o in taxo.dataObjects) {
+				if ("eolMediaURL" in taxo.dataObjects[o]) {
+					// load thumbnail
+					thumbnailURL = taxo.dataObjects[o].eolThumbnailURL;
+					mediaUrl = taxo.dataObjects[o].eolMediaURL;
+					break;
 				}
-			} else {
-				elm = document.querySelector("body#t" + taxonConceptID);
-				if (elm != null) {
-					document.querySelector(".bg").src = taxo[taxonConceptID].dataObjects[0].eolMediaURL;
-					document.querySelector(".fg").src = taxo[taxonConceptID].dataObjects[0].eolMediaURL;
+			}
+		}
+
+		if (ColID == id) {
+			// load main species details
+			if (mediaUrl != null) {
+				document.querySelector(".bg").src = mediaUrl;
+				document.querySelector(".fg").src = mediaUrl;
+			}
+			var names_en = [];
+			for (i = 0; i < taxo.vernacularNames.length; i++) {
+				if (taxo.vernacularNames[i].language == "en") {
+					names_en.push(taxo.vernacularNames[i].vernacularName);
 				}
-				var names_en = [];
-				for (i = 0; i < taxo[taxonConceptID].vernacularNames.length; i++) {
-					if (taxo[taxonConceptID].vernacularNames[i].language == "en") {
-						names_en.push(taxo[taxonConceptID].vernacularNames[i].vernacularName);
-					}
+			}
+			document.getElementById("name-en").textContent = names_en.join(", ");
+		} else {
+			// load child species details
+
+			if (thumbnailURL != null) {
+				var elm = document.querySelector("li#tax-" + ColID);
+				elm.querySelector("img").src = thumbnailURL;
+			}
+
+			var found = false;
+			for (i = 0; i < taxo.vernacularNames.length && !found; i++) {
+				if (taxo.vernacularNames[i].language == "en") {
+					elm.querySelector("h2").textContent += " (" + taxo.vernacularNames[i].vernacularName + ")"
+					found = true;
 				}
-				document.getElementById("name-en").textContent = names_en.join(", ");
 			}
 		}
 	})
+}
+
+/* ================ view routines =================== */
+
+function buildPage(data) {
+	console.log(data);
+	
+	if ("name" in data) {
+		if (id != rootId) {
+			document.querySelector("title").textContent = data.name + " - Biocaching";
+			document.querySelector("h1").textContent = data.name;
+		}
+		document.querySelector("#name").textContent = data.name;
+	}
+
+	var parentItemTemplate, parentItem = null;
+	if ("ancestors" in data) {
+		if (data.ancestors.length > 0) {
+			parentItemTemplate = document.querySelector("#ancestors div");
+			data.ancestors.forEach(function(parent){
+				var item = parentItemTemplate.cloneNode(true);
+				item.querySelector("a").textContent = parent.name;
+				item.querySelector("a").href = uri.setSearch({id: parent.id});
+				if (parentItem == null) {
+					parentItemTemplate.parentNode.appendChild(item);
+				} else {
+					parentItem.appendChild(item);
+				}
+				parentItem = item;
+			});
+			parentItemTemplate.parentNode.removeChild(parentItemTemplate);
+		} else {
+			var ancestors = document.querySelector("#ancestors");
+			ancestors.parentNode.removeChild(ancestors);
+		}
+	}
+
+	var childItemTemplate;
+	if ("children" in data) {
+		childItemTemplate = document.querySelector("#subitems li");
+		data.children.forEach(function(child){
+			var item = childItemTemplate.cloneNode(true);
+			item.querySelector("h2").textContent = child.name;
+			item.querySelector("a").href = uri.setSearch({id: child.id});
+			item.id = "tax-" + child.id;
+			childItemTemplate.parentNode.appendChild(item);
+		});
+		if (data.children.length == 0) {
+			var children = document.querySelector("#subitems");
+			children.parentNode.removeChild(children);
+		} else {
+			childItemTemplate.parentNode.removeChild(childItemTemplate);
+		}
+	}
+
 }
 
 /* ================ initialization =================== */
