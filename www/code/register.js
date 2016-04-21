bc3aba2e46884ff6a90c4557bab496648a5c21db
Peter Bremer <peter.bremer@gmail.com>
2016-04-21 17:08:23 +0200

    Submit selected taxonomy ID, and only allow registrations for items with correct taxonomy ID

--- www/code/register.js
+++ www/code/register.js
@@ -1,3 +1,9 @@
+var query = uri.query(true); // URI.js
+
+function buildPage(data) {
+	document.querySelector("#name").textContent = data.hits[0]._source.scientific_name;
+}
+
 document.querySelector("form").addEventListener("submit", function(e) {
 
 	e.preventDefault();
@@ -5,7 +11,7 @@ document.querySelector("form").addEventListener("submit", function(e) {
 	var file = this.elements["photo-file"].files[0];
 	var data = new FormData();
 	data.append("observation[picture]", file);
-	data.append('observation[taxon_id]', '31619');
+	data.append('observation[taxon_id]', query.id);
 	data.append('observation[observed_at]', '2009-10-26T04:47:09Z');
 	data.append('observation[latitude]', 0);
 	data.append('observation[longitude]', 0); 
@@ -16,8 +22,11 @@ document.querySelector("form").addEventListener("submit", function(e) {
 	xhr.setRequestHeader("X-User-Token", auth.token);
 	xhr.setRequestHeader("X-User-Api-Key", "0b4d859e740d2978b98a13e2b9e130d8");
 
-	xhr.onload = function(e) {
-		alert("Upload finished!");
+	xhr.onload = function() {
+		if (xhr.status == 200)
+			alert("Upload successful!")
+		else
+			alert("Failure: " + xhr.responseText);
 	}
 
 	xhr.send(data);
@@ -39,5 +48,7 @@ document.querySelector('#photo-file').addEventListener('change', function(e) {
 /* ================ initialization =================== */
 
 (function() {
+
+	getData("https://api.biocaching.com/taxa/" + query.id, buildPage)
 
 })();
