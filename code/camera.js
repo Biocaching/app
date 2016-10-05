4c2f93dc887bf5acd4a566542f85385d53bf2320
Peter Bremer <peter.bremer@gmail.com>
2016-10-06 00:20:49 +0200

    Integrate metadata gathering in camera page

--- code/camera.js
+++ code/camera.js
@@ -3,9 +3,23 @@ document.querySelector("#photo-file").addEventListener("change", function(e) {
 	document.querySelector("#display-photo").classList.remove("template");
 	var file = this.files[0];
 	var img = URL.createObjectURL(file);
-	document.querySelector("img").src = img;
-	document.querySelector("img").onload = function() {
+	document.querySelector("#display-photo").src = img;
+	document.querySelector("#display-photo").onload = function() {
 		URL.revokeObjectURL(img);
 	};
+
+	document.querySelector("#controls").classList.add("template");
+	document.querySelector("#location").classList.remove("template");
+	document.querySelector(".pagefooter").classList.remove("template");
+
+	document.querySelector(".timestamp").textContent = (new Date()).toLocaleDateString('nb', {  year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric'});
+	navigator.geolocation.getCurrentPosition(
+		function(loc) {
+			document.querySelector(".coordinates").textContent = loc.coords.latitude + ";" + loc.coords.longitude;
+		},
+		function() {
+			document.querySelector(".coordinates").textContent = 'Unable to get coordinates';
+		}
+	);
 
 }, false);
