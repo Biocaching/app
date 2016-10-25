15d199d73618f3f719a7c68dee9ec6b93bd34fd9
Peter Bremer <peter.bremer@gmail.com>
2016-10-25 12:47:06 +0200

    Map from server data

--- code/map.js
+++ code/map.js
@@ -1,15 +1,47 @@
-(function() {
-	if (document.querySelector(".personal-section"))
-		document.title = "My map - Biocaching";
+function displayData(data) {
+	var map = L.map("map");
+	L.tileLayer(
+		"http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}", {
+	    	attribution: "Map data &copy; <a href='http://openstreetmap.org'>OSM</a> contributors, tiles &copy; <a href='http://korona.geog.uni-heidelberg.de/'>GIScience Heidelberg</a>",
+	    	subdomains: ['a','b','c'],
+	    	maxZoom: 17
+		}
+	).addTo(map);
+	var obsLayer = L.markerClusterGroup({ 
+		showCoverageOnHover: false,
+		maxClusterRadius: 40
+	});
+	data.hits.forEach(function(obs){
+		var m = L.marker([obs._source.location.lat, obs._source.location.lon]);
+		obs = cleanupObservation(obs);
+		var imageCode = "";
+		if (obs.imageUrl) imageCode = "<img src='" + obs.imageUrl + "'/>";
+		m.bindPopup("\
+			<div class='spacer'></div>\
+			" + imageCode + "\
+			<h2>" + obs.commonName + "</h2>\
+			<h3>" + obs.scientificName + "</h3>\
+			<time datetime='2016-11-05'>" + obs.time.toLocaleDateString() + "</time>\
+			");
+		obsLayer.addLayer(m);
+	});
+	map.addLayer(obsLayer);
+	map.fitBounds(obsLayer.getBounds());
+}
 
-	var mymap = L.map('map').setView([69.3, 17.1], 8);
-	L.tileLayer(/*'http://korona.geog.uni-heidelberg.de:8001/tms_r.ashx?x={x}&y={y}&z={z}'*/'http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-	    maxZoom: 18
-	}).addTo(mymap);
-	L.marker([69.4, 17.1]).addTo(mymap)
-		.bindPopup("<div class='spacer'></div><img src='content/observation-3.jpg'/><h2>Norsk navn</h2><h3>Latinsk navn</h3><time datetime='2016-11-05'>11.05.2016</time>").openPopup();
-	L.marker([69.5, 17.9]).addTo(mymap);
-	L.marker([69.0, 17.7]).addTo(mymap);
+(function() {
+
+	if (document.querySelector(".personal-section")) {
+		document.title = "My map - Biocaching";
+		var user = localStorage.getItem("biocaching:user");
+		if (!user)
+			window.location.replace(new URI("signin.html").search({source: uri.toString()}));
+		getData("https://api.biocaching.com/observations?user_id=" + user + "&size=999", displayData)
+	} else {
+		// allow bypassing authorization, since global feed is not private
+		bypassAuthorization();
+
+		getData("https://api.biocaching.com/observations?size=999", displayData)
+	}
 
 })();
