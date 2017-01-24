5b14dcfb1e453e6bd2b651a9ad789c3e0a828922
Peter Bremer <peter.bremer@gmail.com>
2017-01-24 22:52:19 +0100

    Transfer to edit screen after taking a photo

--- code/camera.js
+++ code/camera.js
@@ -10,9 +10,6 @@ document.querySelector("#photo-file").addEventListener("change", function(e) {
 	var file = this.files[0];
 	var img = URL.createObjectURL(file);
 	showPhoto(img, imgDataFormat.URI);
-	document.querySelector("#display-photo").onload = function() {
-		URL.revokeObjectURL(img);
-	};
 
 }, false);
 
@@ -20,67 +17,27 @@ document.querySelector("#photo-file").addEventListener("change", function(e) {
 document.addEventListener("deviceready", function() {
 	// deviceready
 
-	document.querySelector("#photo-button").addEventListener("click", function(evt) {
-
-		var format = imgDataFormat.URI,
-			formatCamera = /*Camera.DestinationType.FILE_URI*/ Camera.DestinationType.NATIVE_URI;
-
-		navigator.camera.getPicture(function(data) {
-			// success
-			showPhoto(data, format);
-		}, function(message) {
-			// failure
-			alert("Failure: " + message);
-		}, {
-			correctOrientation: true, 
-			destinationType: formatCamera
-		});
-		evt.preventDefault();
-	}, false);
-
-	document.querySelector("#browse-button").addEventListener("click", function(evt) {
-		navigator.camera.getPicture(function(data) {
-			// success
-			showPhoto(data, imgDataFormat.URI);
-		}, function(message) {
-			// failure
-			alert("Failure: " + message);
-		}, {
-			//correctOrientation: true, 
-			sourceType: Camera.PictureSourceType.PHOTOLIBRARY
-		});
-	}, false);
+	navigator.camera.getPicture(function(data) {
+		// success
+		showPhoto(data, imgDataFormat.URI);
+	}, function(message) {
+		// failure
+		alert("Failure: " + message);
+	}, {
+		correctOrientation: true, 
+		destinationType: imgDataFormat.URI
+	});
+	evt.preventDefault();
 
 }, false);
 
 function showPhoto(src, dataFmt) {
 
-	var photoElm = document.querySelector("#display-photo");
 	if (dataFmt == imgDataFormat.BASE64) {
-		photoElm.src = "data:image/jpeg;base64," + src;
+		src = "data:image/jpeg;base64," + src;
 	}
-	else {
-		photoElm.src = src;
-	}
-	photoElm.classList.remove("template");
 
-	document.querySelector("#controls").classList.add("template");
-	document.querySelector("#location").classList.remove("template");
-	document.querySelector(".pagefooter").classList.remove("template");
-
-	document.querySelector(".timestamp").textContent = (new Date()).toLocaleDateString('nb', {  year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric'});
-
-	navigator.geolocation.getCurrentPosition(
-		function(loc) {
-			document.querySelector(".coordinates").textContent = loc.coords.latitude + ";" + loc.coords.longitude;
-		},
-		function() {
-			document.querySelector(".coordinates").textContent = 'Unable to get coordinates';
-		},
-		{
-			enableHighAccuracy: true,
-			timeout: 5000
-		}
-	);
+	var dest = URI("edit.html").setSearch({ img: src });
+	window.location.replace(dest);
 
 }
